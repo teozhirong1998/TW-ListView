3f822d3fdc6ee1f74bc8be72d9f589d30f5c76ec
teozhirong1998 <16022635@myrp.edu.sg>
2018-04-24 16:00:14 +0800

    Update commit (First and Second Activity complete)

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -28,6 +31,16 @@ public class FirstActivity extends AppCompatActivity {
         aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, al);
         lv.setAdapter(aa);
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String year = al.get(position);
+                Intent i = new Intent(FirstActivity.this, SecondActivity.class);
 
+                i.putExtra("year", year);
+
+                startActivity(i);
+            }
+        });
     }
 }
